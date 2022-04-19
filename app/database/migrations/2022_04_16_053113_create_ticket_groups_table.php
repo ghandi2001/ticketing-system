fd92a342a49606d3487ec12529377ab7097d2c9f
_ghandi <a.ghandi2001@gmail.com>
2022-04-19 14:45:18 +0000

    debug some migration files and change main port of webservice to 80

--- app/database/migrations/2022_04_16_053113_create_ticket_groups_table.php
+++ app/database/migrations/2022_04_16_053113_create_ticket_groups_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTicketsGroupsTable extends Migration
+class CreateTicketGroupsTable extends Migration
 {
     /**
      * Run the migrations.
--- app/database/migrations/2022_04_16_053358_create_ticket_priorities_table.php
+++ app/database/migrations/2022_04_16_053358_create_ticket_priorities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTicketsPrioritiesTable extends Migration
+class CreateTicketPrioritiesTable extends Migration
 {
     /**
      * Run the migrations.
